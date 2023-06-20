0ad70f8e79ac756c68c454caaf1ff0a2b0ee8605
Masood Acheampong <masoodacheampong@gmail.com>
2023-06-20 22:29:23 +0100

    JokeAPI-project

---
+++ index.js
@@ -0,0 +1,15 @@
+const jokeContent = document.getElementById("joke");
+const btn = document.getElementById("btn");
+const url =
+  "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
+
+let getJoke = () => {
+  fetch(url)
+    .then((data) => data.json())
+    .then((item) => {
+      jokeContent.textContent = `${item.joke}`;
+    })
+    .catch((error) => console.log(error));
+};
+
+btn.addEventListener("click", getJoke);
